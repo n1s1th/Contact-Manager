8166ffeee609b551db98860049b4bb8efac87504
Nisith Moragoda <nisith.nimdinu@gmail.com>
2025-10-12 21:03:24 +0530

    backend

---
+++ contactmanager/src/main/java/com/example/contactmanager/repository/ContactRepository.java
@@ -0,0 +1,8 @@
+package com.example.contactmanager.repository;
+
+import com.example.contactmanager.model.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+    
+}
